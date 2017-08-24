9d08a831a8290ae54a8a89ea917d81efb3692aaa
Elsa Brown <elsambrown@gmail.com>
2017-08-23 20:37:03 -0400

    move index.html to root

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 // app.use('/api', require('./api'));
 
 app.get('/*', (req, res, next) => {
-	res.sendFile(path.join(__dirname, '../public/index.html'))
+	res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 // start server
