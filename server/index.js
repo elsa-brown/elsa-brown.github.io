f382f86abcd0cb0808d5e72ba8c6b2357a47123e
Elsa Brown <elsambrown@gmail.com>
2017-08-23 20:43:01 -0400

    cleaning

--- server/index.js
+++ server/index.js
@@ -6,23 +6,15 @@ const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-// module.exports = app;
-
-// server logging
 app.use(morgan('dev'));
-// req.body parsing
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-// static files
 app.use(express.static(path.join(__dirname, '../public')))
-
-// app.use('/api', require('./api'));
 
 app.get('/*', (req, res, next) => {
 	res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-// start server
 app.listen(8080, () => {
 	console.log('server is listening on 8080')
 })
