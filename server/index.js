a8fd604a98908d1c1422f490dd2d5191a26238a5
Elsa Brown <elsambrown@gmail.com>
2017-08-24 11:28:56 -0400

    move favicon to root

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, './portfolio')))
 
 app.get('/*', (req, res, next) => {
 	res.sendFile(path.join(__dirname, '../index.html'))
